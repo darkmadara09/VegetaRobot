5797de493a2a3bff6b66eeb8e1e130652aa2108c
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-28 16:34:25 +0530

    Update ranks.py

--- VegetaRobot/modules/ranks.py
+++ VegetaRobot/modules/ranks.py
@@ -511,7 +511,7 @@ def devlist(update: Update, context: CallbackContext):
             pass
     update.effective_message.reply_text(reply, parse_mode=ParseMode.HTML)
 
-
+__mod_name__ = "Ranks"
 __help__ = f"""
 *⚠️ Notice:*
 Commands listed here only work for users with special access are mainly used for troubleshooting, debugging purposes.
@@ -602,44 +602,3 @@ Group admins/group owners do not need these commands.
 
 Visit @{SUPPORT_CHAT} for more information.
 """
-
-SUDO_HANDLER = CommandHandler(("addsudo", "addarank"), addsudo, run_async=True)
-SUPPORT_HANDLER = CommandHandler(("addsupport", "addbrank"), addsupport, run_async=True)
-TIGER_HANDLER = CommandHandler(("addtiger" , "addcrank"), addtiger, run_async=True)
-WHITELIST_HANDLER = CommandHandler(("addwhitelist", "adddrank"), addwhitelist, run_async=True)
-UNSUDO_HANDLER = CommandHandler(("removesudo", "removearank"), removesudo, run_async=True)
-UNSUPPORT_HANDLER = CommandHandler(("removesupport", "removebrank"),
-                                   removesupport, run_async=True)
-UNTIGER_HANDLER = CommandHandler(("removetiger" , "removecrank"), removetiger, run_async=True)
-UNWHITELIST_HANDLER = CommandHandler(("removewhitelist", "removedrank"),
-                                     removewhitelist, run_async=True)
-
-WHITELISTLIST_HANDLER = CommandHandler(["whitelistlist", "dranks"],
-                                       whitelistlist, run_async=True)
-TIGERLIST_HANDLER = CommandHandler(["tigers" , "cranks"], tigerlist, run_async=True)
-SUPPORTLIST_HANDLER = CommandHandler(["supportlist", "branks"], supportlist, run_async=True)
-SUDOLIST_HANDLER = CommandHandler(["sudolist", "aranks"], sudolist, run_async=True)
-DEVLIST_HANDLER = CommandHandler(["devlist", "sranks"], devlist, run_async=True)
-
-dispatcher.add_handler(SUDO_HANDLER)
-dispatcher.add_handler(SUPPORT_HANDLER)
-dispatcher.add_handler(TIGER_HANDLER)
-dispatcher.add_handler(WHITELIST_HANDLER)
-dispatcher.add_handler(UNSUDO_HANDLER)
-dispatcher.add_handler(UNSUPPORT_HANDLER)
-dispatcher.add_handler(UNTIGER_HANDLER)
-dispatcher.add_handler(UNWHITELIST_HANDLER)
-
-dispatcher.add_handler(WHITELISTLIST_HANDLER)
-dispatcher.add_handler(TIGERLIST_HANDLER)
-dispatcher.add_handler(SUPPORTLIST_HANDLER)
-dispatcher.add_handler(SUDOLIST_HANDLER)
-dispatcher.add_handler(DEVLIST_HANDLER)
-
-__mod_name__ = "Ranks"
-__handlers__ = [
-    SUDO_HANDLER, SUPPORT_HANDLER, TIGER_HANDLER, WHITELIST_HANDLER,
-    UNSUDO_HANDLER, UNSUPPORT_HANDLER, UNTIGER_HANDLER, UNWHITELIST_HANDLER,
-    WHITELISTLIST_HANDLER, TIGERLIST_HANDLER, SUPPORTLIST_HANDLER,
-    SUDOLIST_HANDLER, DEVLIST_HANDLER
-]
